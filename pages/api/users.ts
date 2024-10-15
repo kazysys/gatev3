fdfa4c5c013b02ba873c6076062873db171fa1d0
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-15 02:31:54 -0300

    Update users.ts

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,15 +1,14 @@
-// pages/api/users.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../lib/dbConnect';
 import User from '../../models/User';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await dbConnect();
-
-    try {
-        const users = await User.find({});
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ message: 'Erro ao buscar usuários.' });
-    }
+  await dbConnect();
+  
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao buscar usuários.' });
+  }
 }
