64bdb479052ea63355bc1260acb051aac22cef87
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-15 02:44:55 -0300

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,19 +1,22 @@
-// lib/dbConnect.ts
 import mongoose from 'mongoose';
 
-const connection = {};
+const connection = { isConnected: false };
 
-// Função para conectar ao banco de dados
 async function dbConnect() {
-    if (connection.isConnected) {
-        return;
-    }
+  if (connection.isConnected) {
+    return;
+  }
 
+  try {
     const db = await mongoose.connect(process.env.MONGODB_URI, {
-        
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
-    connection.isConnected = db.connections[0].readyState;
+    connection.isConnected = db.connection.readyState === 1; // 1 significa conectado
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
 }
 
 export default dbConnect;
