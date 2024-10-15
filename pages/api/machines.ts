147cb14d256da918410567a36b904f2ce9c071ad
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-15 03:21:49 -0300

    Update machines.ts

--- pages/api/machines.ts
+++ pages/api/machines.ts
@@ -1,24 +1,22 @@
+// pages/api/machines.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '../../lib/dbConnect';
 import Machine from '../../models/Machine';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await dbConnect();
-
-  if (req.method === 'POST') {
-    const { ip, username, password } = req.body;
-
-    try {
-      const machine = await Machine.create({ ip, username, password });
-      res.status(201).json(machine);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+    if (req.method === 'POST') {
+        try {
+            const machine = await Machine.create(req.body);
+            res.status(201).json(machine);
+        } catch (error) {
+            // Use type assertion para informar ao TypeScript que 'error' é um objeto
+            const message = (error as { message?: string }).message || 'Erro desconhecido';
+            res.status(400).json({ message });
+        }
+    } else if (req.method === 'GET') {
+        const machines = await Machine.find();
+        res.status(200).json(machines);
+    } else {
+        res.setHeader('Allow', ['GET', 'POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-  } else if (req.method === 'GET') {
-    const machines = await Machine.find();
-    res.status(200).json(machines);
-  } else {
-    res.setHeader('Allow', ['GET', 'POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
 }
