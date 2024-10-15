bb95a0aa954462e1773685a49d9a0c7de334fc43
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-15 02:50:44 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-// pages/_app.tsx
+import { AppProps } from 'next/app'; // Importa os tipos do Next.js
 import '../styles/styles.css'; // Importa o CSS global do Tailwind
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) { // Define os tipos de AppProps
+  return <Component {...pageProps} />;
 }
