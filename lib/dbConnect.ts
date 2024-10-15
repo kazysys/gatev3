cb2d696cf578a17ce5d71a0c59302b6d15545703
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-15 02:28:17 -0300

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -10,8 +10,7 @@ async function dbConnect() {
     }
 
     const db = await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        
     });
 
     connection.isConnected = db.connections[0].readyState;
