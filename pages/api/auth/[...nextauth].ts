5ca4cc71e89d2b6f1ff0318c8b732a04afbf332e
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-15 02:57:29 -0300

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,27 +1,41 @@
-import { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcryptjs';
-import User from '../../../models/User'; // ajuste o caminho conforme necessário
-import dbConnect from '../../../lib/dbConnect'; // ajuste o caminho conforme necessário
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials'; 
+import User from '../../../models/User'; 
+import dbConnect from '../../../lib/dbConnect';
+import bcrypt from 'bcryptjs'; // Importação do bcrypt
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      async authorize(credentials) {
-        await dbConnect();
-
-        if (!credentials) {
-          throw new Error("Credenciais não fornecidas");
+export default NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Usuário", type: "text" },
+                password: { label: "Senha", type: "password" }
+            },
+            async authorize(credentials) {
+                await dbConnect();
+                
+                const user = await User.findOne({ username: credentials.username });
+                if (user && (await bcrypt.compare(credentials.password, user.password))) {
+                    return { id: user._id, name: user.username, email: user.email };
+                }
+                return null;
+            }
+        })
+    ],
+    session: {
+        jwt: true,
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.user.id = token.id;
+            return session;
         }
-
-        const user = await User.findOne({ username: credentials.username });
-
-        if (user && (await bcrypt.compare(credentials.password, user.password))) {
-          return { id: user._id, name: user.username, email: user.email };
-        }
-
-        throw new Error("Nome de usuário ou senha inválidos");
-      },
-    }),
-  ],
-};
+    }
+});
